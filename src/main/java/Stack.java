a424cc3766baea62542b725da0322fc01d6db6f2
Noah Irwin <Noah@Noahs-MBP.lan>
2018-07-22 17:05:24 +0100

    add isEmpty, push, size

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -1,3 +1,17 @@
 public class Stack {
+    private boolean isEmpty = true;
+    private int size = 0;
 
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public void push(Object objectToPush) {
+        isEmpty = false;
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
 }
--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,3 +1,35 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class StackTest {
+    @Test
+    public void testIsEmpty(){
+        Stack emptyStack = new Stack();
+        Stack notEmptyStack = new Stack();
+
+        notEmptyStack.push("Data");
+
+        assertTrue(emptyStack.isEmpty());
+        assertFalse(notEmptyStack.isEmpty());
+    }
+
+    @Test
+    public void testSize(){
+        Stack emptyStack = new Stack();
+        Stack notEmptyStack = new Stack();
+
+        assertEquals(0, emptyStack.size());
+        assertEquals(0, notEmptyStack.size());
+
+        notEmptyStack.push("String");
+        assertEquals(1, notEmptyStack.size());
+
+        notEmptyStack.push("String two");
+        assertEquals(2, notEmptyStack.size());
+    }
+
 
 }
